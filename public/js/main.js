c13406a4d04c3d4563206be1fa3fdce8eff3f37d
Edhi7 <bengans.polare@gmail.com>
2018-04-24 17:32:52 +0200

    V.0.0.2
    Chef har lagatt mat men även en serviceworker och inloggning

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,48 @@
 "use strict";
 
+window.onload = main;
 function main() {
-    const splash = document.getElementById("splash-screen")
-    splash.classList.add("go-away");
+	register_service_worker();
+	check_logged_in();
 }
 
-window.onload = main;
+function register_service_worker() {
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker
+			.register("/js/service_worker.js")
+			.then(function () {
+				console.log("My nam jeff - Hacking ur mom succesful");
+			});
+	}
+}
+
+function check_logged_in() {
+	firebase.auth().onAuthStateChanged((user) => {
+		if (user) {
+			logged_in(user)
+
+		} else {
+			// Display login form
+			// ...
+			display_login();
+		}
+	});
+}
+
+function display_login() {
+	document.getElementById("login").className = "fadeIn";
+	document.getElementById("splash-logo").className = "fadeUp";
+}
+
+function logged_in(user) {
+	// User is signed in.
+	const displayName = user.displayName;
+	const email = user.email;
+	const emailVerified = user.emailVerified;
+	const photoURL = user.photoURL;
+	const isAnonymous = user.isAnonymous;
+	const uid = user.uid;
+	const providerData = user.providerData;
+	const app_bar = document.getElementById("app-bar");
+	app_bar.classList.add("float-up");
+}
